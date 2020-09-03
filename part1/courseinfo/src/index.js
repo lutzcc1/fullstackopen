9990e74b2a3c8fcd77040c99fa54419453189ab4
Luis Cesar <lutzcc1@gmail.com>
2020-09-03 10:23:29 -0500

    exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,46 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name:'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name:'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  } 
+  
 
   return (
     <div>
-      <Header title={course}/>
-      <Content part1={parts[0].name} part2={parts[1].name} part3={parts[2].name} e1={parts[0].exercises} e2={parts[1].exercises} e3={parts[2].exercises}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
 
 const Header = (props) => (
-  <h1>{ props.title }</h1>
+  <h1>{ props.course.name }</h1>
 );
 
 const Content = (props) => (
   <div>
-    <Part part={props.part1} exercises={props.e1}/>
-    <Part part={props.part2} exercises={props.e2}/>
-    <Part part={props.part3} exercises={props.e3}/>
+    <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+    <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+    <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
   </div>
 );
 
-const Total = (props) => (
-  <p>Number of exercises {props.total}</p>
-);
+const Total = (props) => {
+  let total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>
+}
 
 const Part = (props) => (
   <p>{props.part} {props.exercises}</p>
